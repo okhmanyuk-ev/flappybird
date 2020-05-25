2b5b23ed9828b6977da09df02f95e031e044796d
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-05-25 15:18:27 +1000

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
-#include <Platform/defines.h>
-
 #include "application.h"
 
-#if defined(PLATFORM_WINDOWS)
-void main(int argc, char* argv[])
+void sky_main()
 {
 	FlappyBird::Application().run();
 }
-#elif defined(PLATFORM_ANDROID)
-#include <Platform/system_android.h>
-
-void android_main(android_app* app)
-{
-	Platform::SystemAndroid::run(app, [] { FlappyBird::Application().run(); });
-}
-#endif
